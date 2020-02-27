edc153e035ba2b96723b18bc8a52d5ad6c1c8198
CodingDog_zxg <49633098+CodingDogzxg@users.noreply.github.com>
2020-02-28 01:26:00 +0800

    将re匹配版本换为xpath
    节省时间和资源

--- Library/Lib/update.py
+++ Library/Lib/update.py
@@ -1,5 +1,5 @@
 import requests
-import re
+from lxml import etree
 
 
 class Update:
@@ -8,7 +8,7 @@ class Update:
         self.version_info_url = 'https://github.com/CodingDogzxg/Grabber-for-QAU/blob/master/ver_info.zxg'
         self.gotten_response = False
 
-        self.re_found = False
+        self.xpath_match = False
 
         self.cloud_info_dict = {'version': '', 'time': ''}
 
@@ -19,11 +19,23 @@ class Update:
         if re_reponse.status_code == 200:
             self.gotten_response = True
             try:
-                response_content = str(re_reponse.content)[1:-1]
-                re_findall = re.search('#Version#.*#Version#', response_content)
-                cloud_info = re_findall.group()[9:-9].split("#")
+                response_content = str(re_reponse.content)
+                cloud_match_xml = etree.HTML(response_content)
+                cloud_match = cloud_match_xml.xpath('string(// *[ @ id = "LC1"])')
+                cloud_info = cloud_match[9:-9].split("#")
                 self.cloud_info_dict['version'] = cloud_info[0]
                 self.cloud_info_dict['time'] = cloud_info[1]
-                self.re_found = True
+                self.xpath_match = True
             except AttributeError:
-                self.re_found = False
+                self.xpath_match = False
+
+
+            # try:
+            #     response_content = str(re_reponse.content)[1:-1]
+            #     re_findall = re.search('#Version#.*#Version#', response_content)
+            #     cloud_info = re_findall.group()[9:-9].split("#")
+            #     self.cloud_info_dict['version'] = cloud_info[0]
+            #     self.cloud_info_dict['time'] = cloud_info[1]
+            #     self.re_found = True
+            # except AttributeError:
+            #     self.re_found = False
